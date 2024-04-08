dcb26808235bf71d4dc94ab4c44eef00ba718f61
Manuel <manuelloverso60@gmail.com>
2024-04-08 16:22:35 +0200

    add courses section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,7 +9,35 @@ export default {
 <template>
   <main>
     <!-- Browse through courses section -->
-    <section class="courses"></section>
+    <section class="courses">
+      <div class="d-flex container-lg">
+        <!-- Ledt section -->
+        <div class="left col-6">
+          <img width="60" src="/images/Books-icon.png" alt="Books" />
+          <h2 class="fs-7 mb-3">
+            Thousands of courses for any type of student
+          </h2>
+          <p class="fs-3 mb-6">
+            At EduPrime, it doesn't matter what domain you wish to persue a
+            career in. Here you can find a course that satisfies your needs
+            within a click away and applies for a course in a matter of minutes.
+            EduPrime is ranked as the most versatile university in the world,
+            thanks to the number of courses it provides.
+          </p>
+
+          <button class="btn-accent">Browse through courses</button>
+        </div>
+
+        <!-- right section -->
+        <div class="right col-6 d-flex justify-center align-center">
+          <img
+            class="w-80 d-block"
+            src="/images/Graduation-Illustration.png"
+            alt="graduation Hat"
+          />
+        </div>
+      </div>
+    </section>
 
     <!-- EduPrime faculties available section -->
     <section class="faculties"></section>
@@ -36,4 +64,13 @@ export default {
     <section class="partners"></section>
   </main>
 </template>
-<style scoped></style>
+<style scoped>
+main {
+  .courses {
+    padding-top: 4rem;
+    color: var(--text-dark);
+    background-image: url("/images/home-background.png");
+    background-repeat: no-repeat;
+  }
+}
+</style>
